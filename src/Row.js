5d145896aa896af2ca8d3b998c59a1ef745ac04d
Uzair Hussain <uzairhussain98@hotmail.com>
2021-02-04 02:49:18 +0500

    added rows and css

--- src/Row.js
+++ src/Row.js
@@ -1,25 +1,40 @@
 import React, { useState, useEffect} from 'react'
 import axios from './axios';
 import requests from './requests';
+import './Row.css'
 
-const Row = ({ title , fetchUrl}) => {
+const base_URL = "https://image.tmdb.org/t/p/original/"
+
+const Row = ({ title , fetchUrl , isLarge}) => {
     const [movies , setMovies ] = useState([]);
 
     useEffect(()=> {
         async function fetchData(){
             const request = await axios.get(fetchUrl);
-            console.log(request);
+            setMovies(request.data.results)
             return request
 
         }
         fetchData();
 
-    },[]);
+    },[fetchUrl]);
 
   return (
-    <div>
+    <div className="row">
         <h2>{title}</h2>
+
+      <div className="row__posters">
+        {movies.map( movie => (
+          <img className={`row__poster ${isLarge && "row__posterLarge"}` } 
+          key={movie.id}
+              src={`${base_URL}${isLarge ? movie.poster_path : movie.backdrop_path}`} 
+              alt={movie.name}/>
+
+        ))}
+
       
+      </div>
+
     </div>
   )
 }

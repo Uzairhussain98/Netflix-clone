a13a44b4978c975e21e0be14d5a0a3bff29c24bd
Uzair Hussain <uzairhussain98@hotmail.com>
2021-02-10 00:44:02 +0500

    adding video popup

--- src/Row.js
+++ src/Row.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect} from 'react'
 import axios from './axios';
 import requests from './requests';
-import './Row.css'
+import './Row.css';
+import YouTube from 'react-youtube';
 
 const base_URL = "https://image.tmdb.org/t/p/original/"
 
 const Row = ({ title , fetchUrl , isLarge}) => {
     const [movies , setMovies ] = useState([]);
+    const [trailerUrl , setTrailerUrl ] = useState("");
+
 
     useEffect(()=> {
         async function fetchData(){
@@ -19,6 +22,16 @@ const Row = ({ title , fetchUrl , isLarge}) => {
 
     },[fetchUrl]);
 
+    const opts = {
+      height: '390',
+      width: '100%',
+      playerVars: {
+        // https://developers.google.com/youtube/player_parameters
+        autoplay: 1,
+      },
+    };
+
+
   return (
     <div className="row">
         <h2>{title}</h2>
@@ -27,6 +40,7 @@ const Row = ({ title , fetchUrl , isLarge}) => {
         {movies.map( movie => (
           <img className={`row__poster ${isLarge && "row__posterLarge"}` } 
           key={movie.id}
+          onClick={movie}
               src={`${base_URL}${isLarge ? movie.poster_path : movie.backdrop_path}`} 
               alt={movie.name}/>
 
@@ -34,7 +48,7 @@ const Row = ({ title , fetchUrl , isLarge}) => {
 
       
       </div>
-
+        { trailerUrl &&  <YouTube videoId={trailerUrl} opts={opts}/> }
     </div>
   )
 }
